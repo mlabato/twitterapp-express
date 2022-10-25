c1899597f75e5ff1cdefc1cb635d9222ec0013b9
mlabato <m_labato@hotmail.com>
2022-10-25 17:55:22 -0300

    port added

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath)); 
 
 //SERVIDOR LEVANTADO
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
     console.log("http://localhost:3000");
+
+
+
 }); 
+
+
 
 //GUARDADO OBJETOS QUE VIENEN POR POST COMO OBJETOS LITERALES EN JSON
 app.use(express.urlencoded({ extended: false }));
